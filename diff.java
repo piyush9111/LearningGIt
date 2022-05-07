f71df71a71a85e50bf9c723d29fddadf0eef7c3d
Piyush <piyushkhurana23@gmail.com>
2022-05-07 18:51:42 +0530

    Commit with The diff.java file

--- diff.java
+++ diff.java
@@ -1,7 +1,7 @@
-public class sum{
+public class diff{
     public static void main(String[] args){
         int a = 10;
         int b = 10;
-        System.out.println("The sum is = " + a+b);
+        System.out.println("The diff is = " + a-b);
     }
 }
